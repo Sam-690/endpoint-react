d2df4bf123a10992629de18aa9e217520cf1e06f
Sam-690 <56923470+Sam-690@users.noreply.github.com>
2020-03-29 12:16:01 -0600

    a

--- functions/src/correo/Enviar.f.ts
+++ functions/src/correo/Enviar.f.ts
@@ -2,13 +2,19 @@ import * as functions from "firebase-functions";
 import * as cors from "cors";
 import * as nodemailer from 'nodemailer'
 import * as express from "express";
+import * as admin from 'firebase-admin';
+import * as cookieParser from 'cookie-parser';
+
+if(!admin.apps.length){
+    admin.initializeApp();
+}
 
 
 const transportador = nodemailer.createTransport({
     service: 'gmail',
     auth: {
         user: 'sam.martinez690@gmail.com',
-        pass: ''
+        pass: 'xxxxxxxxxxx'
     }
 });
 
@@ -16,30 +22,38 @@ const endPointExpress = express();
 const corsVal = cors({origin:true});
 endPointExpress.options('*', corsVal);
 endPointExpress.use(corsVal);
+endPointExpress.use(cookieParser());
 
-endPointExpress.use(express.json);
+endPointExpress.post('*', async (req, res) =>{
+    try{
+        const _email = req.body.email;
+        const _titulo = req.body.titulo;
+        const _mensaje = req.body.mensaje;
 
-endPointExpress.post('*', (req, res) =>{
-    const _email = req.body.email;
-    const _titulo = req.body.titulo;
-    const _mensaje = req.body.mensaje;
-
-    const emailOpciones = {
-        from: 'sam.martinez.century@gmail.com',
-        to: _email,
-        subject: _titulo,
-        html: '<p>' + _mensaje + '<p>'
-    }
-
-    transportador.sendMail(emailOpciones, function(err, info){
-        if (err){
-            res.send(err);
-        }else{
-            res.send("el email fue enviado correectamente")
+        const emailOpciones = {
+            from: 'sam.martinez.century@gmail.com',
+            to: _email,
+            subject: _titulo,
+            html: '<p>' + _mensaje + '<p>'
         }
-    })
+
+        transportador.sendMail(emailOpciones, function (err, info) {
+            if (err) {
+                res.send(err);
+            } else {
+                res.send("el email fue enviado correectamente")
+            }
+        })
+
+    }
+    catch(e){
+        console.log('error===>', e);
+        res.status(401);
+        res.send(e)
+    }
 })
 
 export = module.exports = functions.https.onRequest((request, response)=>{
     return endPointExpress(request, response);
 })
+
--- functions/src/usuarios/Lista.f.ts
+++ functions/src/usuarios/Lista.f.ts
@@ -4,33 +4,31 @@ import * as express from 'express';
 import * as admin from 'firebase-admin';
 import * as cookieParser from 'cookie-parser';
 
-if(!admin.apps.length){
+if (!admin.apps.length) {
     admin.initializeApp();
 }
 
 const db = admin.firestore();
 
 const endPointExpress = express();
+const corsVal = cors({ origin: true });
 
-const corsVal = cors({origin:true});
-
-endPointExpress.options('*', corsVal);
+endPointExpress.options('*', corsVal)
 endPointExpress.use(corsVal);
 endPointExpress.use(cookieParser());
 
-endPointExpress.get('/lista', async (req: any, res:any) => {
-    const users = db.collection("Users");
+endPointExpress.get('/lista', async (req: any, res: any, next: any) => {
+    const users = db.collection('Users');
     const snapshot = await users.get();
-    const arrayUsers = snapshot.docs.map(doc=>{
+    const arrayUsers = await snapshot.docs.map(doc => {
         const data = doc.data();
         const id = doc.id;
-        return {id, ...data};
-    })
-
+        return { id, ...data }
+    });
     res.status(200);
-    res.json({status:"success", usuarios: arrayUsers})
-})
+    res.json({ status: "success", usuarios: arrayUsers });
+});
 
-exports = module.exports = functions.https.onRequest((request, response)=> {
-    return endPointExpress(request, response)
+exports = module.exports = functions.https.onRequest((request, response) => {
+    return endPointExpress(request, response);
 })
